d44ee75fabbae6b7335ac07ec430345fc31ea2f5
phonaputer <njerome@uwalumni.com>
2026-01-29 20:52:17 +0900

    Switch to writing hello world using SDL logger.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
+#include "SDL3/SDL.h"
 #include <iostream>
 
-int main(int argc, char *argv[]){
-   std::cout << "Hello world" << std::endl;
-
+int main(int argc, char *argv[])
+{
+   SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s", "Hello, world!");
 
    return 0;
 }
